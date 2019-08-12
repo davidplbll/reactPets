6baffa220c18d46cfccd535cf6876844acb01322
DAVID <davidplbll@gmail.com>
2019-08-12 11:31:04 -0500

    .

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCK0MFrkFCMYBzYLIuzcbaSypFbiKYoDB8",
+    authDomain: "mydata-31163.firebaseapp.com",
+    databaseURL: "https://mydata-31163.firebaseio.com",
+    projectId: "mydata-31163",
+    storageBucket: "mydata-31163.appspot.com",
+    messagingSenderId: "117442597002",
+    appId: "1:117442597002:web:d3c2c33dd5664b40"
+  };
+
